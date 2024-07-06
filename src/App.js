a9bbaa7d2a989d18e0ebc1124acd649958db73f0
MonicaAlvesP <monica52782@gmail.com>
2024-07-06 19:45:36 -0300

    regiter memorie

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "Caio Vinícius",
+      idade: 25,
+    }
+  }
+
   render(){
+    // Desestruturação de objetos colocamos o nome da variável entre chaves.
+    const { name } = this.state;
+
     return(
       <div>
         <h1>Olá, tudo certo?</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis nemo unde atque.</p>
+        <p>Hoje, 06/07... Meu filho {name} esta iniciando o curso de Python do Curso em Vídeo.</p>
       </div>
     )
   }
